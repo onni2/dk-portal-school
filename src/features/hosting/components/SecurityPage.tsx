a21fe98e201258ccf40e75c37d067637c6578cff
agustabb <agusta20@ru.is>
2026-03-29 21:18:32 +0000

    feat(hosting): update security page description

--- src/features/hosting/components/SecurityPage.tsx
+++ src/features/hosting/components/SecurityPage.tsx
@@ -11,7 +11,7 @@ export function SecurityPage() {
       title="Öryggi og persónuvernd"
       description="Á þessari síðu finnur þú yfirlit sem útskýrir hvernig dk Hugbúnaður ehf. uppfyllum kröfur ISO 27001, 27017 og 27018. Þar er lögð áhersla á gagnsæi, ábyrgð og örugga meðferð persónuupplýsinga."
     >
-      
+      <p className="text-(--color-text-secondary)">.</p>
     </PageTemplate>
   );
 }
